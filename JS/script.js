d6096823a0e1d7831e2175aac1f33200a15bd3a3
mihir1611 <mihirtrivedigm@yahoo.in>
2020-04-22 13:31:12 +0530

    Toss Application V1 ready

---
+++ JS/script.js
@@ -0,0 +1,73 @@
+alert("hello mihir");
+
+function throwDies(){
+var numb1 = Math.floor(Math.random() * 6) + 1;
+var numb2 = Math.floor(Math.random() * 6) + 1;
+
+if(numb1=="1")
+{
+document.getElementById("firstOutput").innerHTML = "<img src='IMG/one.png'>"
+}
+if(numb1=="2")
+{
+document.getElementById("firstOutput").innerHTML = "<img src='IMG/two.png'>"
+}
+if(numb1=="3")
+{
+document.getElementById("firstOutput").innerHTML = "<img src='IMG/three.png'>"
+}
+if(numb1=="4")
+{
+document.getElementById("firstOutput").innerHTML = "<img src='IMG/four.png'>"
+}
+if(numb1=="5")
+{
+document.getElementById("firstOutput").innerHTML = "<img src='IMG/five.png'>"
+}
+if(numb1=="6")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/six.png'>"
+}
+
+if(numb2=="1")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/one.png'>"
+}
+if(numb2=="2")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/two.png'>"
+}
+if(numb2=="3")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/three.png'>"
+}
+if(numb2=="4")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/four.png'>"
+}
+if(numb2=="5")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/five.png'>"
+}
+if(numb2=="6")
+{
+document.getElementById("secondOutput").innerHTML = "<img src='IMG/six.png'>"
+}
+
+if(numb1==numb2)
+{
+  document.getElementById("firstResult").innerHTML = "Tie"
+  document.getElementById("secondResult").innerHTML = "Tie"
+}
+else if(numb1>numb2)
+{
+document.getElementById("firstResult").innerHTML = "Winner"
+document.getElementById("secondResult").innerHTML = "Looser"
+}
+else if(numb1<numb2){
+  document.getElementById("firstResult").innerHTML = "Looser"
+  document.getElementById("secondResult").innerHTML = "Winner"
+}
+
+
+}
