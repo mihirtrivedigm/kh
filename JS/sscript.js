2002467fa99849ae97af7894c6db690e5366a2ec
mihir1611 <mihirtrivedigm@yahoo.in>
2020-05-22 08:56:47 +0530

    simons patch 2

--- JS/sscript.js
+++ JS/sscript.js
@@ -1,5 +1,38 @@
-$(document).ready(function(){
-  $("btn-primary").click(function(){
-  $("btn-primary").css("background", "green");
+function number1(){
+$("#sound1")[0].play();
+var b = document.getElementById("number2");
+ b.click();
+//$("#number2")[0].click();
+}
+
+$("#number2").click(function(){
+$("#sound2")[0].play();
+alert("number 2 clicked");
 });
-});
+
+ function number2(){
+ $("#sound2")[0].play();
+ }
+
+
+function number3(){
+$("#sound3")[0].play();
+alert("number 3 clicked");
+}
+
+function number4(){
+$("#sound4")[0].play();
+}
+
+function number5(){
+$("#sound5")[0].play();
+}
+
+function number6(){
+$("#sound6")[0].play();
+}
+
+var number = Math.floor(Math.random() * 6) + 1;
+setTimeout(function() { number3(); }, 1000);
+var numberArray = [];
+numberArray.push(number);
